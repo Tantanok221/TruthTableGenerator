ef6bf3074393960e208ecce80df5d5f88bed8b10
Tan Kai <6572withspicy@gmail.com>
2023-11-14 20:22:47 +0800

    Change to function

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Truth Table to HTML(Table) Generator
+This repo use Truth Table Generator to generate a truth table and then use the info to generate a HTML(table) with Pretty Table
+This project at the start is a automation script to deal with school homework
+
+## Installation
+1. ```python -m pip install -U prettytable```
+2. ```pip install truth-table-generator```
+3. Clone this repo to ur computer and have fun!
+
+## Usage
+```
+  generateHTML(table,name) to generate a Truth Table in HTML(table format)
+```
+
+## Dependency
+[Truth Table Generator](https://github.com/chicolucio/truth-table-generator)
--- truth.py
+++ truth.py
@@ -1,9 +1,9 @@
 import ttg
-import prettytable
 
 def generateHTML(table,name):
-  table.format = True
-  newTable = table.get_html_string()
+  oldTable = table.as_prettytable()
+  oldTable.format = True
+  newTable = oldTable.get_html_string()
   # replace all  "or" with "+" and "and" with "*"
   newTable = newTable.replace("or", "+")
   newTable = newTable.replace("and", "*")
@@ -13,41 +13,24 @@ def generateHTML(table,name):
   newTable = newTable.replace("'b", "b'")
   newTable = newTable.replace("'c", "c'")
   newTable = newTable.replace("'d", "d'")
-  # save the html file
-  with open(name + '.html', 'w') as f:
-      f.write(newTable)
-
+  # create a folder call outdir
+  # write the newTable to a html file
+  outdir = "outdir"
+  outfile = open(outdir + "/" + name + ".html", "w")
+  outfile.write(newTable)
+  outfile.close()
+  
+  
+  
+  
+  
+def generateTable(var,condition):
+  table = ttg.Truths(var,condition,ascending = True)
+  return table
 
 # Generate a html file with truth table based on teacher requirement.
 
-threeOriTable = ttg.Truths(["a", "b", "c"], 
-["~a",
-"~b",
-"~c",
-"~a and ~c", # First expressiom
-"~a and ~b", # Second Expression
-"b or ~c", # Third expression (nested without bar)
-"~(b or ~c)",  # Third expression (nested with bar)
-"a and ~(b or ~c)", # Third expression (without nested)
-"~(b and ~(b or ~c))", # Forth expression (without nested)
-"(~a and ~c) or ~b",#Answer
-
-]
-, ascending= True
-).as_prettytable()
-
-threeNewTable = ttg.Truths(["a", "b", "c"], 
-["~a",
-"~b",
-"~c",
-"~a and ~c", # First expressiom
-"(~a and ~c) or ~b"
-
-]
-, ascending= True
-).as_prettytable()
-
-fourOriTable = ttg.Truths(["a", "b", "c", "d"],
+fourOriTable = generateTable(["a", "b", "c", "d"],
 [
   "~b",
   "~c",
@@ -59,14 +42,7 @@ fourOriTable = ttg.Truths(["a", "b", "c", "d"],
   "a and c and ~d",# fifth expression without nested
   "~(a and c and ~d)", # fifth expression with nested
   "(a and ~b and c) or (a and ~b) or (~b and c) or (~b and ~c and ~d) or ~(a and c and ~d)", # answer
-], ascending= True).as_prettytable()
+])
 
-fourNewTable = ttg.Truths(["a", "b", "c", "d"],[
-  "~a",
-  "~b",
-  "~c",
-  "~a or ~b or ~c or d"
-  ],ascending = True).as_prettytable()
-
-
+generateHTML(fourOriTable,"fourOriTable")
 
